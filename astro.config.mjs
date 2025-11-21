ce2799a8154e2744145daa42ea9a652528e9be29
Nicolás Villar <nvillar@marppa.com>
2025-11-21 15:11:15 -0300

    arreglar vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,12 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
+import vercelAdapter from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
+  adapter: vercelAdapter(),
 
   vite: {
     plugins: [tailwindcss()],
